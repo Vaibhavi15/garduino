3e1a47f0114e1f1dd136ef54fb3559d0609d2be3
Vaibhavi15 <paivaibhavi15@gmail.com>
2022-09-21 19:54:10 +0530

    added firebase logic

---
+++ waterplant_firebase.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as gpio
+from time import sleep
+import datetime
+import pyrebase
+import json
+
+CHANNEL = 6
+SECONDS_TO_WATER = 10
+
+with open('/home/pi/garduino/database_config.json', 'r') as config:
+  database_config = json.load(config)
+
+gpio.setmode(gpio.BCM)
+gpio.setup(CHANNEL, gpio.OUT)
+
+firebase = pyrebase.initialize_app(database_config)
+
+database = firebase.database()
+
+def waterPlant():
+    gpio.output(CHANNEL, 0)
+    sleep(SECONDS_TO_WATER)
+    gpio.output(CHANNEL, 1)
+
+now = datetime.datetime.now()
+toWater = database.child("toWater").get().val()
+if(toWater == True):
+    waterPlant()
+    print("Watered plant at: " + now)
+    database.child("toWater").update({"toWater": False})
